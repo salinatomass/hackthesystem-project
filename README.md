0e3efac9ed0bcac10fdbd9747495c8c075a76224
Tomás Salina <salinatomass53@gmail.com>
2021-12-18 07:52:47 -0300

    Setup with webpack

---
+++ README.md
@@ -0,0 +1,24 @@
+# Hack The System Project
+
+Description...
+
+## Gettting Started
+
+1. Clone this project
+2. Go to project's folder
+3. Install dependencies `npm install`
+4. Run and have fun ;) `npm run dev`
+
+## Scripts
+
+- `npm start` to development environment
+- `npm run build` && `npm serve` to production
+
+## Technologies
+
+- React
+- Webpack
+
+## License
+
+MIT
---
+++ webpack.config.dev.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+    },
+  },
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [{ loader: 'html-loader' }],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+};
